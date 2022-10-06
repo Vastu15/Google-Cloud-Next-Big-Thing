a4b29a1c3b3de602b81bd57eb1ff8dbde1700bd0
yuvaraj-07 <yuvarajcoder@gmail.com>
2022-10-06 12:09:02 +0530

    Update openai.py

--- api/routes/openai.py
+++ api/routes/openai.py
@@ -17,7 +17,7 @@ openai_api_router = APIRouter(prefix="/openai/v1", tags=["OpenAI Functions"])
 def get_text_summarizer(text: str = Form(...)):
     response = openai.Completion.create(
         model="text-davinci-002",
-        prompt=f"{text}.\n\nTl;dr",
+        prompt="Many individual performance improvements landed in Python 3.11, but the single biggest addition is the specializing adaptive interpreter. Since an object's type rarely changes, the interpreter now attempts to analyze running code and replace general bytecodes with type-specific ones. For instance, binary operations (add, subtract, etc.) can be replaced with specialized versions for integers, floats, and strings.\n\nPython function calls also require less overhead in Python 3.11. Stack frames for function calls now use less memory and are more efficiently designed. Also, while recursive calls aren't tail-optimized (which probably isn't possible in Python, anyway), they are more efficient than in previous versions. The Python interpreter itself also starts faster, and core modules needed for the Python runtime are stored and loaded more efficiently.\n ",
         temperature=0.7,
         max_tokens=60,
         top_p=1.0,
@@ -44,7 +44,7 @@ def grammer_corrector(text: str = Form(...)):
 @openai_api_router.post("/explain_code")
 def explain_code(text: str = Form(...)):
     response = openai.Completion.create(
-        model="code-davinci-002",
+        model="text-curie-001",
         prompt='class Log:\n    def __init__(self, path):\n        dirname = os.path.dirname(path)\n        os.makedirs(dirname, exist_ok=True)\n        f = open(path, "a+")\n\n        # Check that the file is newline-terminated\n        size = os.path.getsize(path)\n        if size > 0:\n            f.seek(size - 1)\n            end = f.read(1)\n            if end != "\\n":\n                f.write("\\n")\n        self.f = f\n        self.path = path\n\n    def log(self, event):\n        event["_event_id"] = str(uuid.uuid4())\n        json.dump(event, self.f)\n        self.f.write("\\n")\n\n    def state(self):\n        state = {"complete": set(), "last": None}\n        for line in open(self.path):\n            event = json.loads(line)\n            if event["type"] == "submit" and event["success"]:\n                state["complete"].add(event["id"])\n                state["last"] = event\n        return state\n\n"""\nHere\'s what the above class is doing:\n1.',
         temperature=0,
         max_tokens=64,
